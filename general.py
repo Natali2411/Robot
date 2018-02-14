7f61f5d18f9a0043516f791f71d3bc62e1efcebc
Natali2411 <natalka2411>
2018-02-14 23:59:57 +0200

    --create methods for receiving data

--- general.py
+++ general.py
@@ -1,7 +1,5 @@
-import requests
-import json
-import os
-import time, datetime
+import requests, json, os, datetime
+import time, os, hmac, requests, hashlib
 
 class General():
     def getConfig(self):
@@ -14,7 +12,24 @@ class General():
             dt = time.mktime(datetime.datetime.strptime(dateTime, template).timetuple())
         return dt
 
+    def makeAuthentication(self):
+        api = General().getConfig()["gathering"]["base_api"] + '/api/v3/openOrders/'
+        body = ''
+        nonce = int(time.time() * 1e6)
+        API_SECRET = "" #secret
+        ACCESS_KEY = "" #key
+        message = str(nonce) + api + ('' if body is None else body)
+        signature = hmac.new(API_SECRET, message, hashlib.sha256).hexdigest()
+        # Include the signature in the headers
+        headers = {
+            'ACCESS_KEY': ACCESS_KEY,
+            'ACCESS_SIGNATURE': signature,
+            'ACCESS_NONCE': nonce,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'  # Only for POST requests
+        }
+        return api
 
 if __name__ == '__main__':
     obj = General()
-    print(obj.convertDateToTimestamp('2018/02/14 16:45:47'))
+    print(obj.makeAuthentication())
