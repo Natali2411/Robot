6930eb2aa0e09cdd25f9d4e430fb84c62c0ff605
NTyutyunnik <natalka2411>
2018-02-14 16:26:33 +0200

    --binance, creating methods

---
+++ general.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import os
+
+class General():
+    def getConfig(self):
+        filename = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.json'))
+        with open(filename)as f:
+            return json.load(f)
+
+
+if __name__ == '__main__':
+    obj = General()
+    print(obj.getConfig()["gathering"]["base_api"])
