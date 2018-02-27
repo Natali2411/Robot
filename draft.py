d45bdc61e620cf8a9409f9b7ff1ea3744a8e7087
NTyutyunnik <natalka2411>
2018-02-27 17:53:35 +0200

    fewfeqf

--- draft.py
+++ draft.py
@@ -7,11 +7,14 @@ from requests.auth import HTTPBasicAuth
 from zeep.transports import Transport
 import ssl
 
+
 session = Session()
 session.verify = False
-#transport = Transport(session=session)
+transport = Transport(session=session)
 #transport = Transport(http_auth=HTTPBasicAuth("testuser", "testuser"))
-ssl._create_default_https_context = ssl._create_unverified_context
-client = Client('http://kwsesbapu06.alfa.bank.int:8280/services/CardLogisticsWF?wsdl')
-#http_headers = client.wsse.apply(envelope, http_headers)
-result = client.service.getCards(shopId='8344')
+#ssl._create_default_https_context = ssl._create_unverified_context
+client = Client('http://kwsesbapu06.alfa.bank.int:8280/services/CardLogisticsWF?wsdl',
+                transport=transport, strict=False, wsse=UsernameToken("testuser", "testuser"))
+result = client.service.getCardDeliveryStatus(urgent='1')
+# result = client.service.getCards(shopId='8344', cardHash='$1$53553298$8139C5264DDE92403A6695638F9A20CB')
+print(result)
