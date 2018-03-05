573562af067c59a695c76c7f6ea97575b2a20b3c
Natali2411 <natalka2411>
2018-03-06 00:22:41 +0200

    --save

--- draft.py
+++ draft.py
@@ -1,1 +1,25 @@
 # 1. Тебе необходимо написать функцию, которая перевернет массив неопределенной длины. Условие - нельзя использовать встроенный метод length.
+def reverseList(list):
+    l2 = []
+    for i in reversed(list):
+        l2.append(i)
+    return l2
+
+print(reverseList([1,2,3,4,5,6,7,8]))
+
+#2. Реализовать функцию которая отфильтрует все буквы в строке, которые встречаются более одного раза (> 1). Пример: => "agcdcgia": "agc"
+def filtrStr(s):
+    d = {}
+    s2 = ''
+    for i in s:
+        if i not in d:
+            d.setdefault(i, 1)
+        else:
+            d[i] += 1
+            d.update({i: d[i]})
+    for i in d:
+        if d[i] > 1:
+            s2 += i
+    return s2
+
+print(filtrStr('AADDTFGOOFF'))
