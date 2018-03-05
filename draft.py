a656629628703a7d282c9630b9e4b4ce8861b2a8
NTyutyunnik <natalka2411>
2018-03-05 17:47:15 +0200

    fewfeqf

--- draft.py
+++ draft.py
@@ -1,11 +1,1 @@
-for i in range(len(res)):
-    if v_symbol1 == res[i]["symbol"]:
-        r[v_symbol1] = res[i]["askPrice"]
-    if v_symbol2 == res[i]["symbol"]:
-        r[v_symbol2] = res[i]["askPrice"]
-    if v_symbol3 == res[i]["symbol"]:
-        r[v_symbol3] = res[i]["askPrice"]
-    if v_symbol4 == res[i]["symbol"]:
-        r[v_symbol4] = res[i]["askPrice"]
-if len(r) == 2:
-    pass  # rate = float(r[v_symbol2])/float(r[v_symbol1])*float(r[v_symbol3])*float(r[v_symbol4])
+print(int(2.5))
